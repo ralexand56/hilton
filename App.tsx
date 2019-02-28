ad1f31dcf623e93e0334ea0ab2bf719e4e40add3
ralexand56 <ralexand56@live.com>
2019-02-28 10:55:38 -0800

    init

---
+++ App.tsx
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * 
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ * 
+ * @format
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
+});
+
+interface Props {}
+  export default class App extends Component<Props> {
+    render() {
+      return (
+        <View style={styles.container}>
+        {/* <Image source={require('./img/logo_brand_HI.png')} /> */}
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#184D96',
+    color: 'white',
+    padding: 16,
+    height: '100%'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#fff',
+    marginBottom: 5,
+  },
+});
